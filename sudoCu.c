df2d58c7a83e24af99466be8ccb27785f3549ce3
Svenja <goskiatharv@gmail.com>
2024-10-13 23:30:43 +0100

    added board generation first few steps

--- sudoCu.c
+++ sudoCu.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <ncurses.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 int menu() {
     int choice = 0;
@@ -10,6 +12,9 @@ int menu() {
     WINDOW *menuwin = newwin(10, 60, 1, 1);
     box(menuwin, 0, 0);
     keypad(menuwin, TRUE);
+
+    // Hide the cursor
+    curs_set(0);
 
     while (1) {
         mvwprintw(menuwin, 1, 2, "sudoCu");
@@ -37,22 +42,61 @@ int menu() {
                 choice = (choice == 3) ? 0 : choice + 1;
                 break;
             case '\n': // Enter key
+                delwin(menuwin);
                 return choice;
         }
     }
 }
 
-int[9][9] generateBoard() {
-    int board[9][9];
+int (*generateBoard())[9] {
+    static int board[9][9];
+
+    srand(time(NULL));
+    
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            board[i][j] = rand() % (9 - 1 + 1) + 1; // Initialize with some values
+        }
+    }
+
     return board;
 }
 
+void drawSudokuGrid() {
+    // Draw the main grid
+    for (int i = 0; i <= 18; i++) {
+        for (int j = 0; j <= 36; j++) {
+            if (i % 6 == 0 && j % 12 == 0) {
+                mvaddch(i, j, '+');
+            } else if (i % 6 == 0) {
+                mvaddch(i, j, '-');
+            } else if (j % 12 == 0) {
+                mvaddch(i, j, '|');
+            }
+        }
+    }
+}
+
+void drawSudokuNumbers(int (*board)[9]) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            mvprintw(i * 2 + 1, j * 4 + 1, " %d", board[i][j]);
+        }
+    }
+}
+
 void newGame() {
-    board = generateBoard();
+    int (*board)[9] = generateBoard();
+    clear();
+
+    drawSudokuGrid();
+    drawSudokuNumbers(board);
+
+    refresh();
+    getch(); // Wait for user input before returning
 }
 
 int main() {
-    static int board[9][9];
     initscr();
     cbreak();
     noecho();
@@ -65,7 +109,8 @@ int main() {
 
     switch(gameState) {
         case 0:
-            
+            newGame();
+            break;
         case 1:
             printw("Load Game");
             break;
@@ -77,7 +122,10 @@ int main() {
             break;
     }
 
-    endwin();
+    refresh(); // Refresh the standard screen to show the output
+    getch(); // Wait for user input before exiting
+
+    endwin(); // End ncurses mode
 
     return 0;
 }
